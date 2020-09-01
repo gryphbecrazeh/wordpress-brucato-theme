1598a8ea3d58ee5df9b9da281e72a0c76d4934a1
gryphbecrazeh <gryphbecrazeh@gmail.com>
2020-09-01 15:21:45 -0400

    implemented inter post navigation, and automatic redirection to the first post

--- archive.php
+++ archive.php
@@ -1,25 +1,38 @@
-<?php get_header(); ?>
+<?php
+// get_header();
+
+ob_clean();
+ob_start();
+
+$category = get_the_category()[0];
+//   Get all posts in category
+$args = array('category'=> $category->term_id, 'post_type' => 'post');
+$category_post_list = get_posts($args);
+$post_link = get_permalink($category_post_list[0]);
+wp_redirect($post_link);
+exit();
+?>
 <section class="title">
     <h1><?php single_cat_title(); ?></h1>
 </section>
 <section class="archive">
-<?php
+    <?php
 
 if (have_posts()) : while (have_posts()) : the_post();
 ?>
-        <div class="archive-card card bg-dark text-white">
-            <div class="card-body">
-                <h3><?php the_title(); ?></h3>
-                <?php
+    <div class="archive-card card bg-dark text-white">
+        <div class="card-body">
+            <h3><?php the_title(); ?></h3>
+            <?php
                 the_excerpt();
                 ?>
-            </div>
-            <div class="card-footer">
-                <a href="<?php the_permalink(); ?>" class='btn btn-warning d-block'>Read More...</a>
-
-            </div>
         </div>
-<?php
+        <div class="card-footer">
+            <a href="<?php the_permalink(); ?>" class='btn btn-warning d-block'>Read More...</a>
+
+        </div>
+    </div>
+    <?php
     endwhile;
 endif;
 
--- single.php
+++ single.php
@@ -10,73 +10,109 @@ $pdf_viewer = get_field("pdf_viewer");
 $additional_image_carousel = get_field("additional_image_carousel");
 $postId = get_the_ID();
 $category = get_the_category($postId)[0];
+
+
+function find_index_from_array($key, $query, $array) {
+  foreach($array as $array_index=>$value) {
+    if(strval($value->$key) == $query) {
+      return $array_index;
+    }
+  }
+}
+
+
+
+
+
+//   Get all posts in category
+$args = array('category'=> $category->cat_ID, 'post_type' => 'post');
+$post_list = get_posts($args);
+
+$current_post_index = find_index_from_array('ID', $postId, $post_list);
+$previous_post = $post_list[$current_post_index - 1];
+$next_post = $post_list[$current_post_index + 1];
+
+// print_r($previous_post);
+// echo '<br/><br/>';
+// print_r($next_post);
+// echo $next_post->guid;
+// die();
 ?>
 
 <?php get_header(); ?>
 <section class="brucato-post">
-  <div class="brucato-category-list">
-    <span class="category"><?php echo $category->name; ?></span>
-    <ol>
-      <?php
-    //   Get all posts in category
-        $args = array('category'=> $category->cat_ID, 'post_type' => 'post');
-        $post_list = get_posts($args);
-        foreach ($post_list as $index=>$post) : setup_postdata($post);
-      ?>
-      <li>
-            <a href="<?php the_permalink(); ?>">
-             <?php echo $index+1; ?>
-          </a>
-      </li>
-      <?php endforeach; ?>
-    </ol>
-  </div>
-  <div class="brucato-heading">
-    <div class="brucato-title">
-      <h1>
-      <?php
-    //   var_export($active_post);
-    //   die();
-      echo $active_post->post_title;?>
-      </h1>
+    <div class="brucato-category-list">
+        <span class="category"><?php echo $category->name; ?></span>
+        <ol>
+            <?php
+foreach ($post_list as $index=>$post) : setup_postdata($post);
+?>
+            <li>
+                <a href="<?php the_permalink(); ?>">
+                    <?php echo $index+1; ?>
+                </a>
+            </li>
+            <?php endforeach; ?>
+        </ol>
     </div>
-    <div class="brucato-description">
-    <?php echo $post_description; ?>
+    <div class="brucato-heading">
+        <div class="brucato-title">
+            <h1>
+                <?php
+//   var_export($active_post);
+//   die();
+echo $active_post->post_title;?>
+            </h1>
+        </div>
+        <div class="brucato-description">
+            <?php echo $post_description; ?>
+        </div>
     </div>
-  </div>
-  <div class="featured-image">
-    <?php echo $post_featured_image; ?>
-  </div>
-  <div class="brucato-carousel">
-    <?php echo $image_carousel; ?>
-  </div>
-  <div class="brucato-pdf-viewer">
-  <?php echo $pdf_viewer; ?>
-  </div>
-  <div class="brucato-post-body">
-<?php
+    <div class="featured-image">
+        <?php echo $post_featured_image; ?>
+    </div>
+    <div class="brucato-carousel">
+        <?php echo $image_carousel; ?>
+    </div>
+    <div class="brucato-pdf-viewer">
+        <?php echo $pdf_viewer; ?>
+    </div>
+    <div class="brucato-post-body">
+        <?php
 
 if (have_posts()) : while (have_posts()) : the_post();
 
-        the_content();
-    endwhile;
+the_content();
+endwhile;
 endif;
 
 
 ?>
-  </div>
+    </div>
     <div class="brucato-carousel">
-    <?php echo $additional_image_carousel; ?>
-  </div>
+        <?php echo $additional_image_carousel; ?>
+    </div>
 
-  <div class="brucato-post-navigation">
-    <div class="previous">
-      <a href="">Prev</a>
+    <div class="brucato-post-navigation">
+        <div class="previous">
+            <?php
+          if($previous_post) {
+            ?>
+            <a href="<?php echo get_the_permalink($previous_post); ?>">Prev</a>
+            <?php
+          }
+        ?>
+        </div>
+        <div class="next">
+            <?php
+          if($next_post) {
+            ?>
+            <a href="<?php echo get_the_permalink($next_post); ?>">Next</a>
+            <?php
+          }
+        ?>
+        </div>
     </div>
-    <div class="next">
-      <a href="">Next</a>
-    </div>
-  </div>
 </section>
 
 
